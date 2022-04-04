fe3f0d9dae8a4b71a467e41b3662d27f0f4200ea
MathRoda <mouawiahammo65@gmail.com>
2022-04-04 23:43:20 +0200

    ok

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import math
+import random
+
 import cvzone
 import cv2
 import numpy as np
@@ -9,6 +12,65 @@ cap.set(4, 720)
 
 
 detector = HandDetector(detectionCon=0.8, maxHands=1)
+
+class SnakeGameAi:
+
+    def __init__(self, pathFood):
+        self.points = [] # all points of the snake
+        self.lenghts = [] # Distance between each point
+        self.currentLength = 0 # total length of the snake
+        self.allowedLength = 150 # Total allowed length
+        self.previousHead = 0, 0 # previous head point
+
+        self.imageFood = cv2.imread(pathFood, cv2.IMREAD_UNCHANGED)
+        self.hFood, self.wFood, _ = self.imageFood.shape
+        self.foodPoints = 0, 0
+        self.randomFoodLocation()
+
+    def randomFoodLocation(self):
+        self.foodPoints = random.randint(100, 1000), random.randint(100, 600)
+
+    def update(self, imgMain, currentHead):
+        px, py = self.previousHead
+        cx, cy = currentHead
+
+        self.points.append([cx, cy])
+        distance = math.hypot(cx - px , cy - py)
+        self.lenghts.append(distance)
+        self.currentLength += distance
+        self.previousHead = cx, cy
+
+        # Length Reduction
+        if self.currentLength > self.allowedLength:
+            for i, length in enumerate(self.lenghts):
+                self.currentLength -= length
+                self.lenghts.pop(i)
+                self.points.pop(i)
+
+                if self.currentLength < self.allowedLength:
+                    break
+        # check if the snake ate the food
+        rx, ry = self.foodPoints
+        if rx - self.wFood//2 < cx < rx + self.wFood//2 and \
+                ry - self.hFood//2 < cy < ry + self.hFood//2 :
+            self.randomFoodLocation()
+            self.allowedLength += 50
+
+        # Draw snake
+        if self.points:
+           for i, point in enumerate(self.points):
+               if i != 0:
+                  cv2.line(imgMain, self.points[i-1], self.points[i], (0, 0, 255), 20 )
+           cv2.circle(img, self.points[-1], 20, (200, 0, 200), cv2.FILLED)
+
+        # Draw Food
+        rx, ry  = self.foodPoints
+        imgMain = cvzone.overlayPNG(imgMain, self.imageFood, (rx-self.wFood//2, ry-self.hFood//2))
+        return imgMain
+
+game = SnakeGameAi("libya.png")
+
+
 while True:
     success, img = cap.read()
     img = cv2.flip(img, 1)
@@ -17,7 +79,7 @@ while True:
     if hands:
         lmList = hands[0]['lmList']
         pointIndex = lmList[8][0:2]
-        cv2.circle(img, pointIndex, 20, (200, 0, 200), cv2.FILLED)
+        img = game.update(img, pointIndex)
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
